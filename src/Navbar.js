114448517c98d62a618ba979839d6e00eed76d4e
“tejas-srinivas” <stejas2002@gmail.com>
2023-09-18 19:36:10 +0530

    Adapted to Mobile Screen

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-import logo from '../src/grc.png'
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../src/grc.png";
 
 const Navbar = () => {
   return (
-    <Link to='/'>
-      <div className='navbar'>
-        <div className='nav__container'>
-          <img src={logo} className='icon' alt=''/>
-          <h1>Crypto <span className='purple'>Tracker</span></h1>
+    <div className="navbar">
+      <div className="nav__container">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} className="icon" alt="" />
+            <h1>
+              Crypto  <span className="purple">Tracker</span>
+            </h1>
+          </Link>
+        </div>
+        <div className="navigate-elements">
+          <ul>
+            <li><Link to='https://react.dev/learn'>React JS</Link></li>
+            <li><Link to='https://www.chartjs.org/docs/latest/'>Chart JS</Link></li>
+            <li><Link to='https://www.coingecko.com/en/api/documentation'>API</Link></li>
+          </ul>
         </div>
       </div>
-    </Link>
+    </div>
   );
-}
+};
 
 export default Navbar;
